640f38b9122b3bded226a083f62f450464f744ad
TuanDao2002 <tuan09092002@gmail.com>
2022-06-19 12:06:51 +0700

    add skill and experience

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About"
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Home />
             <About />
             <Skills />
+            <Experience />
         </div>
     )
 }
